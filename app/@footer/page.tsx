848ebf97c20b3a83568b0fae02c538c505ecb7ae
dhanuram99 <dhanuram99@gmail.com>
2023-07-30 22:20:53 +0530

    Initial setup

---
+++ app/@footer/page.tsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            Foot
+        </footer>
+    )    
+}
+
+export default Footer;
---
+++ app/@header/page.tsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <header>
+            head
+        </header>
+    )    
+}
+
+export default Header;
