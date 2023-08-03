34b658c491593196bb21a668d7805bd632a6979f
dhanuram99 <dhanuram99@gmail.com>
2023-08-03 22:46:06 +0530

    Animation fixes

--- app/_sections/about/about.tsx
+++ app/_sections/about/about.tsx
@@ -11,9 +11,30 @@ function About(){
     useEffect(()=>{
         gsap.registerPlugin(ScrollTrigger)
         const tweens : gsap.core.Tween[] = [];
+
         tweens.push(
             gsap.to(containerRef.current,{css:{visibility:'visible'},duration:0})        
         )
+
+        //Title animation
+        tweens.push(
+            gsap.from(
+                [containerRef.current?.children[0]],
+                {
+                    x:'-100',
+                    opacity: 0,
+                    scrollTrigger: {
+                        trigger: containerRef.current,
+                        start: "top 95%",
+                        end: "bottom 95%",
+                    },
+                    duration: 0.8,
+                    ease: Power3.easeOut,
+                }
+            )
+        )
+
+        //Skill card animation
         const children:number = containerRef.current?.children[1].children.length || 0;
         for (let i = 0; i < children; i++) {
             const child = containerRef.current?.children[1].children[i];
@@ -21,15 +42,15 @@ function About(){
                 gsap.from(
                     [child],
                     {
-                        x:'-50%',
+                        x:'-300px',
                         opacity: 0,
                         scrollTrigger: {
                             trigger: child,
-                            start: "top 95%",
-                            end: "bottom 95%",
+                            start: "top 98%",
+                            end: "bottom 98%",
                         },
-                        duration: 0.4,
-                        ease: Power3.easeInOut,
+                        duration: 0.8,
+                        ease: Power3.easeOut,
                     }
                 )
             )
