168d664099dfa8928ae430e8de9e594a1f767090
dhanuram99 <dhanuram99@gmail.com>
2023-08-04 23:25:04 +0530

    Footer

--- app/@footer/page.tsx
+++ app/@footer/page.tsx
@@ -1,6 +1,6 @@
 function Footer(){
     return(
-        <footer className="bg-secondary px-[5%] text-[white] border-[#eaeaea] border-t border-solid text-center">
+        <footer className="bg-secondary px-[5%] text-[black] border-[#eaeaea] border-t border-solid text-center font-medium">
             <div className="py-5">
                 DHANAVEL @ <span className="text-primary">2023</span>
             </div>
