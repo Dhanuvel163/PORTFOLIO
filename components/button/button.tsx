8d964a838c1c0f6ea9438325f6fb89df319ec624
dhanuram99 <dhanuram99@gmail.com>
2023-08-08 01:10:05 +0530

    dark op

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -6,7 +6,7 @@ function Button({
     className?: string, icon?: JSX.Element, title?: string, label:string, onClick?:MouseEventHandler<HTMLButtonElement>
 }){
     return(
-        <button className={`flex items-center pointer-events-auto rounded-md px-4 py-2 text-center font-medium shadow-sm hover:shadow ring-1 ring-[rgb(51,65,85)]/[0.1] dark:ring-[rgb(255,255,255)]/[0.3] hover:bg-[hsl(240,4.8%,95.9%)] dark:hover:!bg-darkprimary/30 hover:opacity-70 ${className}`} aria-label={label} onClick={onClick}>
+        <button className={`flex items-center pointer-events-auto rounded-md px-4 py-2 text-center font-medium shadow-sm hover:shadow ring-1 ring-[rgb(51,65,85)]/[0.1] dark:ring-[rgb(255,255,255)]/[0.3] hover:bg-[hsl(240,4.8%,95.9%)] dark:hover:!bg-darkprimary/30 dark:hover:opacity-70 ${className}`} aria-label={label} onClick={onClick}>
             {icon}
             {
                 title && 
