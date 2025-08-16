92f43576af6a4f4911a4b121233ec5dde069bd4b
Dhanavel <dhanuram99@gmail.com>
2025-08-16 08:37:00 +0530

    Fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: '/projects', destination: '/' },
+    ]
+  },
+}
 
 module.exports = nextConfig
