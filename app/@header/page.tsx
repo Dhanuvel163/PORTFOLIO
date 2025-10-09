9527a92e5fd575af7c3203f99b094bd78c83627f
dhanuram9999 <dhanuram9999@gmail.com>
2025-10-10 02:34:15 +0530

    Updated page.tsx

--- app/@header/page.tsx
+++ app/@header/page.tsx
@@ -6,6 +6,8 @@ import {MdLightMode} from 'react-icons/md';
 import { ThemeContext } from '../@darkmode/darkmode';
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineNightlight } from "react-icons/md";
+import { FaHome, FaProjectDiagram } from 'react-icons/fa';
+import Link from 'next/link';
 
 interface HeaderProps {
   isOpen: boolean;
@@ -22,10 +24,31 @@ function Header({ isOpen, onClose = ()=>{}, onOpen = ()=>{} }: HeaderProps){
                     <Image src={'/images/logo/logo100x100.webp'} layout='fill' objectFit='cover' alt='logo'/>
                 </div>
             </div>
-            <div className='hidden 200:flex dark:text-[white]'>
+
+            <div className='hidden 1000:flex items-center gap-6 dark:text-[white]'>
+                <Link href="/" className='flex items-center gap-2 hover:text-primary transition-colors'>
+                    <FaHome className='text-lg'/>
+                    <span className='font-medium'>Home</span>
+                </Link>
+                <Link href="/projects" className='flex items-center gap-2 hover:text-primary transition-colors'>
+                    <FaProjectDiagram className='text-lg'/>
+                    <span className='font-medium'>Projects</span>
+                </Link>
+                <div className='ml-4'>
+                    {
+                        theme?.theme
+                        ? <Button className="dark:hover:!bg-darkprimary/30 font-bold min-[600px]:py-3 px-0 flex justify-center items-center w-8 h-8 min-[600px]:w-10 min-[600px]:h-10 !rounded-full"
+                            icon={<MdLightMode/>} label='Dark' onClick={()=>theme.toggleTheme()}/>
+                        : <Button className="dark:hover:!bg-darkprimary/30 font-bold min-[600px]:py-3 px-0 flex justify-center items-center w-8 h-8 min-[600px]:w-10 min-[600px]:h-10 !rounded-full"
+                        icon={<MdOutlineNightlight className='-rotate-[30deg]'/>} label='Light' onClick={()=>theme?.toggleTheme()}/>
+                    }
+                </div>
+            </div>
+
+            <div className='flex 1000:hidden dark:text-[white]'>
                 <div className='m-auto ml-2 280:ml-4'>
                     {
-                        theme?.theme 
+                        theme?.theme
                         ? <Button className="dark:hover:!bg-darkprimary/30 font-bold min-[600px]:py-3 px-0 flex justify-center items-center w-8 h-8 min-[600px]:w-10 min-[600px]:h-10 !rounded-full"
                             icon={<MdLightMode/>} label='Dark' onClick={()=>theme.toggleTheme()}/>
                         : <Button className="dark:hover:!bg-darkprimary/30 font-bold min-[600px]:py-3 px-0 flex justify-center items-center w-8 h-8 min-[600px]:w-10 min-[600px]:h-10 !rounded-full"
